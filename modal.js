5dda89a00c10b3491735ecf59b8fa928f524021e
jpp7557 <jp75.tran@gmail.com>
2024-10-05 11:02:56 +0200

    update index.html modal.css modal.js

--- modal.js
+++ modal.js
@@ -1,14 +1,20 @@
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const formContent = document.querySelector(".content");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeDialogBtn = document.querySelector('.close');
-//const checkboxL1 = document.querySelector("label[for='checkbox1']");
-// launch modal event
+
+// event that launches the subscription form modal 
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+/* ********************************************************* */
+/*  click sur le signe X pour fermer le modal de formulaire  */
+/* ********************************************************* */
+
 closeDialogBtn.addEventListener('click', () => {
-  modalbg.style.display = "none"; // Close the dialog
+  formContent.style.display = "none"; // Close the form
+  modalbg.style.display = "none";     // Close the dialog
 });
 
 //****  my script  BEGIN =====>
@@ -25,13 +31,13 @@ const checkbox1Message = document.getElementById("checkbox1Msg");
 const quantityInput = document.getElementById('quantity');
 const quantityError = document.getElementById('quantityError');
 const locationError = document.getElementById('locationError');
+const radios = document.querySelectorAll('input[name="location"]');
 const merciMessage = document.getElementById('merciMessage');
 const merciPopup = document.getElementById('merciPopup');
 const formulaire = document.getElementById('formulaire');
-
+const merciClose = document.getElementById('merciClose');
 // Regular expression for email validation
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
 
 function editNav() {
 
@@ -46,132 +52,10 @@ function editNav() {
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  formContent.style.display = "block"; // display form content
 }
 
 // validation functions
-
-function validateEmail() {
-  const emailValue = emailInput.value.trim();
-
-  // Check if the email format is valid
-  if (emailRegex.test(emailValue)) {
-    emailError.style.display = 'none'; // Hide the error message if the format is valid
-    return true;
-  } else {
-    emailError.style.display = 'block';  // Show the error message if the format is invalid
-    return false;
-  }
-}
-
-function validateAcception() {
-
-  if (checkbox1.checked) {
-    checkbox1Message.style.display = "none";
-  } else {
-    checkbox1Message.style.display = "block";
-  }
-  return checkbox1.checked;
-}
-
-function validateBirthDate() {
-
-    const re = /^(\d{4})-(\d{2})-(\d{2})$/;
-
-    let birthdateValue = birthdateInput.value;
-    let match = re.exec(birthdateValue); // if valid date:  result ["yyyy-mm-dd", "yyyy", "mm", "dd"]
-
-    if (!match) {
-      birthdateError.style.display = "block";
-      return false;
-    }
-
-    let year = parseInt(match[1], 10); // sting to int conversion
-    let month = parseInt(match[2], 10);
-    let day = parseInt(match[3], 10);
-
-    let userBirthdate = new Date(year, month - 1, day); // Create a Date object with user provided birthday
-    let today = new Date();
-    let m = today.getMonth() - userBirthdate.getMonth();
-
-    //age calculation
-    let age = today.getFullYear() - userBirthdate.getFullYear();
-    //ajust age from month and Date 
-    if (m < 0 || (m === 0 && today.getDate() < userBirthdate.getDate())) {
-        age--;
-    }
-
-    if (age >= 18) {
-      birthdateError.style.display = "none";
-      return true;
-    }
-
-    birthdateError.style.display = "block";
-    return false;
-
-  }
-
-  function participationNumber() {
-    
-      let pNumberVal = quantityInput.value;
-
-      if ((pNumberVal == "") || (pNumberVal < 0) || (pNumberVal > 99) ) {
-        quantityError.style.display = "block";
-        return false;
-      }
-      quantityError.style.display = "none";
-      return true;  
-  }
-
-  function isLocationChosen() {
-
-    let radios = document.querySelectorAll('input[name="location"]');
-    let locationSelected = false;
-
-    locationError.style.display = 'block'; // set display
-
-    for (const radio of radios) {
-      if (radio.checked) {
-        locationSelected = true;
-        locationError.style.display = 'none'; // Hide the error message if the format is valid
-        break;
-      } 
-    };
-    return locationSelected;
-  }
-
-// add more script ***** BEGIN
-
-function wait4Merci(event, form) {
-  event.preventDefault(); // Prevent form submit
-  form.classList.add('inactive-form');
-  closeDialogBtn.classList.add('inactive-form');
-
-
-  // This function simulates a loop
-  let loop = setInterval(() => {
-    if (!isLooping) {
-      clearInterval(loop);  // Break the loop by clearing the interval
-      // Resume form submission here after the loop is stopped
-      form.submit();  // Submit the form
-    }
-  }, 1000);  // Loop every 1000 ms
-
-}
-
-
-
-// Add an event listener to the closeMerci button to resume Form submit
-document.getElementById('closeMerci').addEventListener('click', () => {
-
-    formulaire.classList.remove('inactive-form');
-    closeDialogBtn.classList.remove('inactive-form');
-    merciPopup.style.display = 'none';   // Close merci popup
-    modalbg.style.display = 'none';   // Close modal popup
-
-    isLooping = false;  // Set the flag to false to stop the loop
-
-});
-
 
 function nameCheck(uName) {
 
@@ -201,8 +85,115 @@ function lastnameCheck() {
   return nameCheck(name);
 }
 
-// add more script ***** END
+function validateEmail() {
+  const emailValue = emailInput.value.trim();
 
+  // Check if the email format is valid
+  if (emailRegex.test(emailValue)) {
+    emailError.style.display = 'none'; // Hide the error message if the format is valid
+    return true;
+  } else {
+    emailError.style.display = 'block';  // Show the error message if the format is invalid
+    return false;
+  }
+}
+
+function validateAcception() {
+
+  if (checkbox1.checked) {
+    checkbox1Message.style.display = "none";
+  } else {
+    checkbox1Message.style.display = "block";
+  }
+  return checkbox1.checked;
+}
+
+function validateBirthDate() {
+
+    const re = /^(\d{4}).(\d{2}).(\d{2})$/;
+
+    let birthdateValue = birthdateInput.value;
+
+    if (birthdateValue === "") {
+      birthdateError.style.display = "block";
+      return false;
+    }
+
+    let match = re.exec(birthdateValue); // if valid date:  match = ["yyyy-mm-dd", "yyyy", "mm", "dd"]
+    let year = parseInt(match[1], 10); // sting to int conversion
+    let month = parseInt(match[2], 10);
+    let day = parseInt(match[3], 10);
+
+    let userBirthdate = new Date(year, month - 1, day); // Create a Date object with user provided birthday
+    let today = new Date();
+
+    //age calculation
+    let age = today.getFullYear() - userBirthdate.getFullYear();
+
+    //ajust age from month and Date 
+    let m = today.getMonth() - userBirthdate.getMonth();
+
+    if (m < 0 || (m === 0 && today.getDate() < userBirthdate.getDate())) {
+        age--;
+    }
+
+    if (age >= 18) {
+      birthdateError.style.display = "none";
+      return true;
+    }
+
+    birthdateError.style.display = "block";
+    return false;
+
+  }
+
+  function participationNumber() {
+    
+      let pNumberVal = quantityInput.value;
+
+      if ((pNumberVal == "") || (pNumberVal < 0) || (pNumberVal > 99) ) {
+        quantityError.style.display = "block";
+        return false;
+      }
+      quantityError.style.display = "none";
+      return true;  
+  }
+
+  function isLocationChosen() {
+
+    let locationSelected = false;
+
+    locationError.style.display = 'block'; // set display
+ 
+    for (let radio of radios) {
+      if (radio.checked) {
+        locationSelected = true;
+        locationError.style.display = 'none'; // Hide the error message if the format is valid
+        break;
+      } 
+    };
+    return locationSelected;
+  }
+
+  function displayFormValues(form) {
+    for (let element of form.elements) {
+      if (element.type === "submit") 
+        continue;  
+
+      if (element.id === "checkbox1") 
+        continue;
+  
+      if (element.type === "radio" && element.checked) {
+        console.log(`${element.name}: ${element.value}`);
+      }
+      else if (element.id === "checkbox2") {
+        console.log(`${element.id}: ${element.checked}`);
+      }
+      else if (element.type !== "radio") {
+        console.log(`${element.id}: ${element.value}`);
+      }
+    }
+  }
 
 // Checking valid format while quitting the input fields
 firstnameInput.addEventListener('blur', firstnameCheck);
@@ -211,21 +202,29 @@ emailInput.addEventListener('blur', validateEmail);
 birthdateInput.addEventListener('blur', validateBirthDate);
 quantityInput.addEventListener('blur', participationNumber);
 
+const myFormulaire = document.getElementById('formulaire');
+
+/***************************************************/
+/*  radio buttons and terms of use event handling  */
+/***************************************************/
+
+myFormulaire.addEventListener('change', (event) => {
+  if (event.target.name === 'location') {
+      locationError.style.display = 'none';
+  } else if (event.target.id === 'checkbox1') {
+      validateAcception();
+  }
+});
 
 
 /* ********************* */
 /*  Form Sumit handling  */
 /* ********************* */
 
-let isLooping = true;  // tag for wait4Merci() , init value 
-const myFormulaire = document.getElementById('formulaire');
-
 //  Form Submit event Listening ...
-myFormulaire.addEventListener("submit", (event) => {
+myFormulaire.addEventListener('submit', (event) => {
 
-    // Get form values
-    //const checkbox = document.querySelector('#checkbox1');
-
+    event.preventDefault();  // pas de submit, même si pas d'erreur (formatError = false) !
 
     let notValidFirstName = !firstnameCheck();
     let notValidLastName  = !lastnameCheck();
@@ -238,16 +237,40 @@ myFormulaire.addEventListener("submit", (event) => {
     let formatError = notValidFirstName || notValidLastName || notValidEmail ||
                       notValidBirthDate || nbParticipation  || notValidLocation || notAccepted;
 
-    // Form submit if no error
-    if (formatError) {
-        event.preventDefault();
-    } else {
-        //alert("Merci !");
-        merciPopup.style.display = 'block';
-        merciMessage.textContent = "Merci pour votre réservation !";
-        wait4Merci(event, myFormulaire);
+    // format checking, si pas d'erreur, fermer le formulaire, affiche merci 
+    if (!formatError) {
+        formContent.style.display = "none"; // Close formulaire
+        merciPopup.style.display = 'flex';
+        merciPopup.style.flexDirection = 'column';
+        merciMessage.textContent = "Merci pour votre inscription !";
+        displayFormValues(myFormulaire);
+        myFormulaire.reset();
+
     }
 })
+
+/* ************************* */
+/*  click sur bouton fermer  */
+/* ************************* */
+
+// Add an event listener to the merciFermer button to resume Form submit
+document.getElementById('merciFermer').addEventListener('click', () => {
+
+  merciPopup.style.display = 'none';  // Close merci popup
+  modalbg.style.display = 'none';     // Close modal popup
+
+});
+
+
+/* ************************************************* */
+/*  click sur le signe X du modal merci pour fermer  */
+/* ************************************************* */
+
+document.getElementById('merciClose').addEventListener('click', () => {
+  merciPopup.style.display = 'none';  // Close merci popup
+  modalbg.style.display = 'none';     // Close modal popup
+});
+
 
 
 
